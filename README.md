f51e3ab640fb9712b284609b9dc60ea61c0daa59
Adam Nguyen <adamdnguyen10@gmail.com>
2020-05-19 16:30:38 -0700

    feat: add help command

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # agent-anon
 Discord bot for Project Winter
+
+## Invite bot to server
+- Visit: https://discord.com/api/oauth2/authorize?client_id=712204217909248010&permissions=522304&scope=bot
+- Select your Discord server
+- Type `!anon help` for usage guide
--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function Session(isActive, playerVotes = {}, playerNum = 0, channel = null) {
     this.playerNum = playerNum;
     this.channel = channel;
 
-    this.playersLeft = function() {
+    this.playersLeft = function () {
         return this.playerNum - Object.keys(playerVotes).length;
     };
 
-    this.isSameTeam = function() {
+    this.isSameTeam = function () {
         return Object.values(this.playerVotes).every((val, i, arr) => val === arr[0]);
     };
 }
@@ -22,18 +22,25 @@ let globalSession = new Session(false, {});
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setActivity("!anon help", {
+        type: "PLAYING"
+    });
 })
+
 client.on("message", msg => {
     const author = msg.author.username;
     const playerNum = parseInt(msg.content.charAt(7)) || 0;
     const channelID = msg.channel.id;
     const messageType = msg.channel.type;
     const sanitizedInput = msg.content.toLowerCase();
+    const memberJoin = msg.type === "GUILD_MEMBER_JOIN";
 
     // Game ending
     function finalizeGame() {
         const text = globalSession.isSameTeam() ? colorGreen(`Hooray! You guys are on the same team!!`) : colorRed(`You guys are NOT on the same team.`);
+        const partingText = "Contact <@396131916098568192> or visit https://github.com/nad0m/agent-anon for more bot details and features.";
         globalSession.channel.send(text);
+        globalSession.channel.send(partingText);
         globalSession = new Session(false, {}, 0, client.channels.cache.get(channelID));
     }
 
@@ -44,6 +51,7 @@ client.on("message", msg => {
         globalSession = new Session(true, {}, playerNum, client.channels.cache.get(channelID));
         globalSession.channel.send(colorBlue(newGameText));
         globalSession.channel.send(instructText);
+
     }
 
     // During game
@@ -64,6 +72,18 @@ client.on("message", msg => {
             globalSession.channel.send("Current session erased. Type !start `# of players` in channel to start new session.");
         }
         globalSession = new Session(false, {}, 0, client.channels.cache.get(channelID));
+    }
+
+    // Display help message
+    else if (sanitizedInput === "!anon help" && messageType === 'text') {
+        const text = colorYellow("Begin session: !start {playerAmount}\nex: !start 4\nThis begins the session with 4 players.");
+        client.channels.cache.get(channelID).send(text);
+    }
+
+    else if (memberJoin) {
+        const details = "For more details about this bot and its creator, contact <@396131916098568192> or visit https://github.com/nad0m/agent-anon";
+        client.channels.cache.get(channelID).send("Hi, I'm Agent Anon. Type `!anon help` on how to use."); 
+        client.channels.cache.get(channelID).send(details);
     }
 })
 
